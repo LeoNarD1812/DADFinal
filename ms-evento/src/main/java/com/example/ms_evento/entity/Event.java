5404ca80ec6f0daad7a4f9752762b301fedd710e
LeoNarD1812 <jesus.huaman181219@gmail.com>
2024-11-24 03:39:24 -0500

    ...

--- ms-evento/src/main/java/com/example/ms_evento/entity/Event.java
+++ ms-evento/src/main/java/com/example/ms_evento/entity/Event.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDate;
 
 @Entity
 @Data
@@ -15,8 +14,6 @@ public class Event {
     private String name;
     private String organizador;
     private String place;
-    private Double capacity;
-    private LocalDate date;
     private String description;
     private String estado;
     private String code;
